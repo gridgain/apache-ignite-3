e2398319764e61d4a29f2f7b8fab0c47f7721704
Vladislav Pyatkov <vldpyatkov@gmail.com>
2021-04-05 18:40:56 +0300

    Moved classes to correct package.

---
+++ modules/runner/src/main/java/org/apache/ignite/IgniteImpl.java
@@ -0,0 +1,67 @@
+package org.apache.ignite;
+
+import java.util.function.Consumer;
+import org.apache.ignite.configuration.ConfigurationModuleImpl;
+import org.apache.ignite.internal.table.distributed.TableManagerImpl;
+import org.apache.ignite.lang.LogWrapper;
+import org.apache.ignite.table.Table;
+import org.apache.ignite.table.distributed.configuration.DistributedTableConfiguration;
+import org.apache.ignite.table.distributed.configuration.TableInit;
+import org.apache.ignite.table.manager.TableManager;
+
+/**
+ * Ignite interface.
+ */
+public class IgniteImpl implements Ignite {
+    /** Logger. */
+    LogWrapper log = new LogWrapper(IgniteImpl.class);
+
+    /** Configuration module. */
+    private ConfigurationModuleImpl configurationModule;
+
+    /** Table manager implementation. */
+    private TableManagerImpl tableManagerImpl;
+
+    /**
+     * @param tableManagerImpl Table manager implementation.
+     * @param configurationModule Configuration module.
+     */
+    public IgniteImpl(TableManagerImpl tableManagerImpl, ConfigurationModuleImpl configurationModule) {
+        this.tableManagerImpl = tableManagerImpl;
+        this.configurationModule = configurationModule;
+    }
+
+    /** {@inheritDoc} */
+    @Override public Table createTable(String name, Consumer<TableInit> tableInitChange) {
+        configurationModule.configurationRegistry()
+            .getConfiguration(DistributedTableConfiguration.KEY).tables().change(change ->
+            change.create(name, tableInitChange));
+
+//        this.createTable("tbl1", change -> {
+//            change.initBackups(2);
+//            change.initName("tbl1");
+//            change.initPartitions(1_000);
+//        });
+
+        //TODO: Get it honestly using some future.
+        Table tbl = null;
+
+        while (tbl == null) {
+            try {
+                Thread.sleep(50);
+
+                tbl = tableManagerImpl.table(name);
+            }
+            catch (InterruptedException e) {
+                log.error("Waiting of creation of table was interrupted.", e);
+            }
+        }
+
+        return tbl;
+    }
+
+    /** {@inheritDoc} */
+    @Override public TableManager tableManager() {
+        return tableManagerImpl;
+    }
+}
