26249ad490775bc69d8cebd54e9268bab03886d3
vladimirermakov <vladermakov07@gmail.com>
2021-11-10 17:06:16 +0530

    IGNITE-15846 Refactored InternalTable.java

--- modules/calcite/src/main/java/org/apache/ignite/internal/processors/query/calcite/extension/OnlyTableImpl.java
+++ modules/calcite/src/main/java/org/apache/ignite/internal/processors/query/calcite/extension/OnlyTableImpl.java
@@ -2,22 +2,15 @@ package org.apache.ignite.internal.processors.query.calcite.extension;
 
 import java.util.List;
 
-import org.apache.calcite.config.CalciteConnectionConfig;
 import org.apache.calcite.plan.RelOptCluster;
 import org.apache.calcite.plan.RelOptTable;
 import org.apache.calcite.plan.RelTraitSet;
-import org.apache.calcite.rel.RelCollation;
 import org.apache.calcite.rel.core.TableScan;
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
-import org.apache.calcite.schema.Schema;
-import org.apache.calcite.schema.Statistic;
-import org.apache.calcite.sql.SqlCall;
-import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.util.ImmutableBitSet;
 import org.apache.ignite.internal.processors.query.calcite.extension.api.InternalTable;
 import org.apache.ignite.internal.processors.query.calcite.extension.api.InternalTableDescriptor;
-import org.apache.ignite.internal.processors.query.calcite.rel.logical.IgniteLogicalTableScan;
 import org.apache.ignite.internal.processors.query.calcite.trait.IgniteDistribution;
 import org.apache.ignite.internal.processors.query.calcite.trait.IgniteDistributions;
 import org.apache.ignite.internal.processors.query.calcite.type.IgniteTypeFactory;
@@ -37,54 +30,17 @@ class OnlyTableImpl implements InternalTable {
     }
     
     /** {@inheritDoc} */
-    @Override public RelDataType getRowType(RelDataTypeFactory typeFactory) {
-        return desc.rowType((IgniteTypeFactory) typeFactory, null);
+    @Override public RelDataType getRowType(RelDataTypeFactory typeFactory, ImmutableBitSet requiredColumns) {
+        return desc.rowType((IgniteTypeFactory) typeFactory, requiredColumns);
     }
     
-    @Override
-    public RelDataType getRowType(RelDataTypeFactory typeFactory, ImmutableBitSet requiredColumns) {
-        return null;
-    }
-    
-    /** {@inheritDoc} */
-    @Override public Statistic getStatistic() {
-        return new Statistic() {
-            @Override public @Nullable List<RelCollation> getCollations() {
-                return List.of();
-            }
-        };
-    }
-
-    /** {@inheritDoc} */
-    @Override public Schema.TableType getJdbcTableType() {
-        return Schema.TableType.TABLE;
-    }
-
-    /** {@inheritDoc} */
-    @Override public boolean isRolledUp(String column) {
-        return false;
-    }
-
-    /** {@inheritDoc} */
-    @Override public boolean rolledUpColumnValidInsideAgg(String column, SqlCall call, @Nullable SqlNode parent,
-        @Nullable CalciteConnectionConfig config) {
-        return false;
-    }
-
-    @Override public TableScan toRel(RelOptTable.ToRelContext context, RelOptTable relOptTable) {
-        RelOptCluster cluster = context.getCluster();
-
+    @Override public TableScan toRel(RelOptCluster cluster, RelOptTable relOptTable) {
         RelTraitSet traits = cluster.traitSetOf(MyConvention.INSTANCE)
             .replace(IgniteDistributions.broadcast());
 
         return new MyPhysTableScan(
-            context.getCluster(), traits, List.of(), relOptTable
+            cluster, traits, List.of(), relOptTable
         );
-    }
-    
-    @Override
-    public IgniteLogicalTableScan toRel(RelOptCluster cluster, RelOptTable relOptTbl) {
-        return null;
     }
     
     @Override
--- modules/calcite/src/main/java/org/apache/ignite/internal/processors/query/calcite/extension/api/InternalTable.java
+++ modules/calcite/src/main/java/org/apache/ignite/internal/processors/query/calcite/extension/api/InternalTable.java
@@ -1,15 +1,22 @@
 package org.apache.ignite.internal.processors.query.calcite.extension.api;
 
+import java.util.List;
+import org.apache.calcite.config.CalciteConnectionConfig;
 import org.apache.calcite.plan.RelOptCluster;
 import org.apache.calcite.plan.RelOptTable;
+import org.apache.calcite.rel.RelCollation;
 import org.apache.calcite.rel.core.TableScan;
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeFactory;
+import org.apache.calcite.schema.Schema;
+import org.apache.calcite.schema.Statistic;
 import org.apache.calcite.schema.TranslatableTable;
 import org.apache.calcite.schema.Wrapper;
+import org.apache.calcite.sql.SqlCall;
+import org.apache.calcite.sql.SqlNode;
 import org.apache.calcite.util.ImmutableBitSet;
-import org.apache.ignite.internal.processors.query.calcite.rel.logical.IgniteLogicalTableScan;
 import org.apache.ignite.internal.processors.query.calcite.trait.IgniteDistribution;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 public interface InternalTable extends TranslatableTable, Wrapper {
     /**
@@ -43,10 +50,36 @@ public interface InternalTable extends TranslatableTable, Wrapper {
      * @param relOptTbl Table.
      * @return Table relational expression.
      */
-    IgniteLogicalTableScan toRel(RelOptCluster cluster, RelOptTable relOptTbl);
+    TableScan toRel(RelOptCluster cluster, RelOptTable relOptTbl);
     
     /**
      * @return Table distribution.
      */
     IgniteDistribution distribution();
+    
+    /** {@inheritDoc} */
+    default Schema.TableType getJdbcTableType() {
+        return Schema.TableType.TABLE;
+    }
+    
+    /** {@inheritDoc} */
+    default boolean isRolledUp(String column) {
+        return false;
+    }
+    
+    /** {@inheritDoc} */
+    default boolean rolledUpColumnValidInsideAgg(String column, SqlCall call, @Nullable SqlNode parent,
+            @Nullable CalciteConnectionConfig config) {
+        return false;
+    }
+    
+    /** {@inheritDoc} */
+    default Statistic getStatistic() {
+        return new Statistic() {
+            @Override public @Nullable List<RelCollation> getCollations() {
+                return List.of();
+            }
+        };
+    }
+    
 }
