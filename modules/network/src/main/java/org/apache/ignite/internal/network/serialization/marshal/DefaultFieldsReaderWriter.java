f2578c4eb6886680cc21a1ef213e5ca913528c10
Roman Puchkovskiy <roman.puchkovskiy@gmail.com>
2022-01-12 11:36:45 +0400

    - add javadocs

--- modules/network/src/main/java/org/apache/ignite/internal/network/serialization/marshal/DefaultFieldsReaderWriter.java
+++ modules/network/src/main/java/org/apache/ignite/internal/network/serialization/marshal/DefaultFieldsReaderWriter.java
@@ -23,12 +23,33 @@ import java.io.IOException;
 import org.apache.ignite.internal.network.serialization.ClassDescriptor;
 
 /**
- *
+ * Knows how to read/write fields in the 'default way' (that is, the way that is default for User Object Serialization,
+ * as opposed to user customizations possible via writeObject()/readObject() methods).
  */
 interface DefaultFieldsReaderWriter {
+    /**
+     * Writes object fields to the output stream using default marshalling.
+     *
+     * @param object     object which fields to write
+     * @param descriptor object class descriptor
+     * @param output     output to write to
+     * @param context    marshalling context
+     * @throws MarshalException if something goes wrong
+     * @throws IOException      if I/O fails
+     */
     void defaultWriteFields(Object object, ClassDescriptor descriptor, DataOutputStream output, MarshallingContext context)
             throws MarshalException, IOException;
 
+    /**
+     * Reads object fields from the input stream and stores them in the object using default marshalling.
+     *
+     * @param input      input from which to read
+     * @param object     object to fill
+     * @param descriptor object class descriptor
+     * @param context    unmarshalling context
+     * @throws IOException          if I/O fails
+     * @throws UnmarshalException   if something goes wrong
+     */
     void defaultFillFieldsFrom(DataInputStream input, Object object, ClassDescriptor descriptor, UnmarshallingContext context)
             throws IOException, UnmarshalException;
 }
