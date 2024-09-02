9c1dce8fe5b01b81e98ff7bda14ccd92510420a2
Alexey Scherbakov <alexey.scherbakoff@gmail.com>
2024-09-02 17:16:13 +0300

    IGNITE-22835 Shared disruptor wip.

--- modules/raft/src/main/java/org/apache/ignite/raft/jraft/core/NodeImpl.java
+++ modules/raft/src/main/java/org/apache/ignite/raft/jraft/core/NodeImpl.java
@@ -1435,7 +1435,9 @@ public class NodeImpl implements Node, RaftServerService {
                 postfix = ownFsmCallerExecutorDisruptorConfig.getThreadPostfix();
             }
 
-            StripedDisruptor<SharedEvent> sharedDisruptor = new StripedDisruptor<>(
+            // TODO use shared distuptor property.
+            StripedDisruptor<SharedEvent> sharedDisruptor =
+                opts.getfSMCallerExecutorDisruptor() == null || ownFsmCallerExecutorDisruptorConfig != null ? new StripedDisruptor<>(
                             opts.getServerName(),
                             "JRaft-Shared-Disruptor" + postfix,
                             clo,
@@ -1445,7 +1447,7 @@ public class NodeImpl implements Node, RaftServerService {
                             false,
                             false,
                             opts.getRaftMetrics().disruptorMetrics("raft.shared.disruptor")
-                        );
+                        ) : null; // Avoid creating useless disruptor.
 
             if (opts.getfSMCallerExecutorDisruptor() == null || ownFsmCallerExecutorDisruptorConfig != null) {
                 opts.setfSMCallerExecutorDisruptor((StripedDisruptor<IApplyTask>) (StripedDisruptor<? extends IApplyTask>) sharedDisruptor);
