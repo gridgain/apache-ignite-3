b089f94fc4470efdead10f6e77a9b9a105a5a3f2
denis-chudov <moonglloom@gmail.com>
2024-09-03 10:19:06 +0300

    benchmark improvement*

--- modules/table/src/integrationTest/java/org/apache/ignite/TxRetryBenchmark.java
+++ modules/table/src/integrationTest/java/org/apache/ignite/TxRetryBenchmark.java
@@ -1,12 +1,11 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * Copyright 2019 GridGain Systems, Inc. and Contributors.
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the GridGain Community Edition License (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gridgain.com/products/software/community-edition/gridgain-community-edition-license
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,127 +16,168 @@
 
 package org.apache.ignite;
 
-import static org.apache.ignite.internal.testframework.IgniteTestUtils.waitForCondition;
-import static org.apache.ignite.internal.util.FastTimestamps.coarseCurrentTimeMillis;
+import static org.apache.ignite.internal.catalog.CatalogService.DEFAULT_STORAGE_PROFILE;
 
-import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import org.apache.ignite.internal.ClusterPerTestIntegrationTest;
+import org.apache.ignite.internal.TestWrappers;
 import org.apache.ignite.internal.app.IgniteImpl;
-import org.apache.ignite.internal.app.IgniteServerImpl;
-import org.apache.ignite.internal.testframework.TestIgnitionManager;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Param;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.TearDown;
-import org.openjdk.jmh.annotations.Threads;
-import org.openjdk.jmh.annotations.Warmup;
-import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.apache.ignite.sql.IgniteSql;
+import org.apache.ignite.table.RecordView;
+import org.apache.ignite.table.Tuple;
+import org.apache.ignite.tx.IgniteTransactions;
+import org.apache.ignite.tx.MismatchingTransactionOutcomeException;
+import org.apache.ignite.tx.Transaction;
+import org.junit.jupiter.api.Test;
 
-/**
- * Feature timeout benchmark.
- */
-@State(Scope.Benchmark)
-@Fork(1)
-@Threads(1)
-@Warmup(iterations = 10, time = 2)
-@Measurement(iterations = 20, time = 2)
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.MICROSECONDS)
-public class TxRetryBenchmark {
-    private IgniteImpl ignite;
-
-    /**
-     * Prepare to start the benchmark.
-     */
-    @Setup
-    public void setUp() {
-        String cfgString = "ignite {\n"
-                + "  network.port: 3344,\n"
-                + "  network.nodeFinder.netClusterNodes: [localhost\":\"3344]\n"
-                + "  network.membership: {\n"
-                + "    membershipSyncInterval: 1000,\n"
-                + "    failurePingInterval: 500,\n"
-                + "    scaleCube: {\n"
-                + "      membershipSuspicionMultiplier: 1,\n"
-                + "      failurePingRequestMembers: 1,\n"
-                + "      gossipInterval: 10\n"
-                + "    },\n"
-                + "  },\n"
-                + "  raft: {\n"
-                + "  fsync: false,\n"
-                + "  retryDelay: 20\n"
-                + "},\n"
-                + "  clientConnector.port: 10800,\n"
-                + "  storage: {\n"
-                + "    profiles: {default_aimem: { engine: \"aimem\"}}\n"
-                + "  },\n"
-                + "  rest: {\n"
-                + "    port: 10300, \n"
-                + "    ssl.port: 10400 \n"
-                + "  },\n"
-                + "  nodeAttributes: {    nodeAttributes: {}  }}";
-
-        String nodeName = "node0";
-        Path workDir = workDir();
-
-        IgniteServer igniteServer = TestIgnitionManager.start(
-                nodeName,
-                cfgString,
-                workDir.resolve(nodeName)
-        );
-
-        InitParameters initParameters = InitParameters.builder()
-                .metaStorageNodeNames(nodeName)
-                .clusterName("cluster")
-                .build();
-
-        TestIgnitionManager.init(igniteServer, initParameters);
-
-        ignite = (IgniteImpl) igniteServer.api();
+public class TxRetryBenchmark extends ClusterPerTestIntegrationTest {
+    @Override
+    protected int initialNodes() {
+        return 3;
     }
 
-    private Path workDir() {
-        return Path.of("build", "work")
-                .resolve(this.getClass().getSimpleName())
-                .resolve("work_" +  ThreadLocalRandom.current().nextInt(Short.MAX_VALUE));
+    private IgniteImpl anyNode() {
+        return runningNodes().map(TestWrappers::unwrapIgniteImpl).findFirst().orElseThrow();
     }
 
-    /**
-     * Closes resources.
-     */
-    @TearDown
-    public void tearDown() throws InterruptedException, ExecutionException {
-        ignite.stop();
+    @Test
+    public void test() throws ExecutionException, InterruptedException {
+        int keysUpperBound = 10;
+
+        IgniteImpl ignite = anyNode();
+
+        IgniteSql sql = ignite.sql();
+
+        String zoneName = "TEST_ZONE";
+        String tableName = "TEST";
+
+        sql.execute(null, String.format("CREATE ZONE IF NOT EXISTS %s WITH REPLICAS=%d, PARTITIONS=%d, STORAGE_PROFILES='%s'",
+                zoneName, initialNodes(), 10, DEFAULT_STORAGE_PROFILE));
+        sql.execute(null, "CREATE TABLE IF NOT EXISTS " + tableName
+                + "(id INT PRIMARY KEY, name VARCHAR) WITH PRIMARY_ZONE='" + zoneName + "';");
+
+        for (int i = 0; i < keysUpperBound; i++) {
+            sql.execute(null, "INSERT INTO " + tableName + "(id, name) VALUES (?, ?)", i, "val_" + i);
+        }
+
+        RecordView recordView = ignite.tables().table(tableName).recordView();
+
+        measure(60_000, recordView, ignite.transactions(), keysUpperBound);
+        measure(60_000, recordView, ignite.transactions(), keysUpperBound);
+
+        measure(60_000, recordView, ignite.transactions(), keysUpperBound);
     }
 
-    /**
-     * Benchmark for KV upsert via embedded client.
-     */
-    @Benchmark
-    public void test() {
+    private void measure(
+            long time,
+            RecordView view,
+            IgniteTransactions transactions,
+            int keyUpperBound
+    ) throws InterruptedException, ExecutionException {
+        int cores = Runtime.getRuntime().availableProcessors();
 
+        long startTime = System.currentTimeMillis();
+        long untilTime = startTime + time;
+
+        List<AtomicLong> txCounters = new ArrayList<>();
+        List<AtomicLong> opsTimes = new ArrayList<>();
+        List<AtomicInteger> rolledBackTxnsList = new ArrayList<>();
+
+        for (int i = 0; i < cores; i++) {
+            txCounters.add(new AtomicLong());
+            opsTimes.add(new AtomicLong());
+            rolledBackTxnsList.add(new AtomicInteger());
+        }
+
+        ExecutorService executor = Executors.newFixedThreadPool(cores);
+
+        List<Future> futures = new ArrayList<>();
+
+        for (int i = 0; i < cores; i++) {
+            AtomicLong txCounter = txCounters.get(i);
+            AtomicLong opsTime = opsTimes.get(i);
+            AtomicInteger rolledBackTxns = rolledBackTxnsList.get(i);
+            futures.add(executor.submit(() -> doTxs(view, transactions, untilTime, keyUpperBound, txCounter, opsTime, rolledBackTxns)));
+        }
+
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        executor.shutdown();
+        executor.awaitTermination(10, TimeUnit.SECONDS);
+        for (Future f : futures) {
+            f.get();
+        }
+
+        long totalTxs = txCounters.stream().mapToLong(AtomicLong::get).sum();
+        long totalDuration = opsTimes.stream().mapToLong(AtomicLong::get).sum();
+        int rolledBackTxns = rolledBackTxnsList.stream().mapToInt(AtomicInteger::get).sum();
+
+        double avgDuration = totalDuration / 1_000_000.0 / totalTxs;
+
+        System.out.println("total txns: " + totalTxs);
+        System.out.println("rolled back txns: " + rolledBackTxns);
+        System.out.printf("avg upsert duration: %f\n", avgDuration);
     }
 
-    /**
-     * Benchmark's entry point.
-     */
-    public static void main(String[] args) throws RunnerException {
-        Options opt = new OptionsBuilder()
-                .include(".*" + TxRetryBenchmark.class.getSimpleName() + ".*")
-                .build();
+    private void doTxs(
+            RecordView view,
+            IgniteTransactions transactions,
+            long untilTimeout,
+            int keyUpperBound,
+            AtomicLong txCounter,
+            AtomicLong opsTime,
+            AtomicInteger rolledBackTxns
+    ) {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        long currentTime = System.currentTimeMillis();
 
-        new Runner(opt).run();
+        while (currentTime < untilTimeout) {
+            Transaction tx = transactions.begin();
+
+            int key = random.nextInt(keyUpperBound);
+
+            try {
+                long upsertStart = System.nanoTime();
+                view.upsert(tx, Tuple.create().set("id", key).set("name", "val_" + random.nextInt(keyUpperBound)));
+                long upsertDuration = System.nanoTime() - upsertStart;
+                opsTime.addAndGet(upsertDuration);
+            } catch (Exception e) {
+                System.out.println("qqq " + e);
+
+                boolean rolledBack = true;
+                try {
+                    tx.rollback();
+                } catch (MismatchingTransactionOutcomeException ex) {
+                    rolledBack = false;
+                }
+
+                if (rolledBack) {
+                    rolledBackTxns.incrementAndGet();
+                }
+            }
+
+            try {
+                tx.commit();
+            } catch (MismatchingTransactionOutcomeException ex) {
+                rolledBackTxns.incrementAndGet();
+            }
+
+            currentTime = System.currentTimeMillis();
+
+            txCounter.incrementAndGet();
+        }
     }
 }
