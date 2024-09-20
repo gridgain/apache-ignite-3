724c3955bc16594aaf8025bf49510216edd761d9
Alexey Scherbakov <alexey.scherbakoff@gmail.com>
2024-09-20 17:41:53 +0300

    IGNITE-22835 Debug wip 21.

--- modules/table/src/main/java/org/apache/ignite/internal/table/distributed/schema/SchemaSyncServiceImpl.java
+++ modules/table/src/main/java/org/apache/ignite/internal/table/distributed/schema/SchemaSyncServiceImpl.java
@@ -48,6 +48,8 @@ public class SchemaSyncServiceImpl implements SchemaSyncService {
 //        }
 //        return clusterTime.waitFor(ts.subtractPhysicalTime(delayDurationMs.getAsLong()));
 
+        ts.subtractPhysicalTime(delayDurationMs.getAsLong());
+
         return nullCompletedFuture();
     }
 }
