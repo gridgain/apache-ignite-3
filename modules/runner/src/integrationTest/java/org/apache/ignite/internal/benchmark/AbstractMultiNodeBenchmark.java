cbbf53a3e0a00f21e33d224ba4a30e292d38a5f9
Alexey Scherbakov <alexey.scherbakoff@gmail.com>
2024-09-20 14:34:16 +0300

    IGNITE-22835 Debug wip 6.

--- modules/runner/src/integrationTest/java/org/apache/ignite/internal/benchmark/AbstractMultiNodeBenchmark.java
+++ modules/runner/src/integrationTest/java/org/apache/ignite/internal/benchmark/AbstractMultiNodeBenchmark.java
@@ -79,7 +79,7 @@ public class AbstractMultiNodeBenchmark {
      */
     @Setup
     public void nodeSetUp() throws Exception {
-        System.setProperty("jraft.available_processors", "2");
+        //System.setProperty("jraft.available_processors", "2");
         startCluster();
 
         try {
