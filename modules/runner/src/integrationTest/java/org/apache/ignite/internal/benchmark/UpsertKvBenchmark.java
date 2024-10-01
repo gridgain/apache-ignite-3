e8b4334aa013ed49ea76f07d6606e5e5c6049921
Alexey Scherbakov <alexey.scherbakoff@gmail.com>
2024-10-01 13:33:34 +0300

    IGNITE-22835 Debug coarse lock 5

--- modules/runner/src/integrationTest/java/org/apache/ignite/internal/benchmark/UpsertKvBenchmark.java
+++ modules/runner/src/integrationTest/java/org/apache/ignite/internal/benchmark/UpsertKvBenchmark.java
@@ -72,8 +72,8 @@ public class UpsertKvBenchmark extends AbstractMultiNodeBenchmark {
     @Override
     public void nodeSetUp() throws Exception {
         //System.setProperty(IgniteSystemProperties.IGNITE_USE_SHARED_EVENT_LOOP, "true");
-        //System.setProperty(IgniteSystemProperties.IGNITE_SKIP_REPLICATION_IN_BENCHMARK, "true");
-        System.setProperty(IgniteSystemProperties.IGNITE_SKIP_STORAGE_UPDATE_IN_BENCHMARK, "true");
+        System.setProperty(IgniteSystemProperties.IGNITE_SKIP_REPLICATION_IN_BENCHMARK, "true");
+        //System.setProperty(IgniteSystemProperties.IGNITE_SKIP_STORAGE_UPDATE_IN_BENCHMARK, "true");
         super.nodeSetUp();
 
         //igniteImpl.metricManager().enable("raft");
