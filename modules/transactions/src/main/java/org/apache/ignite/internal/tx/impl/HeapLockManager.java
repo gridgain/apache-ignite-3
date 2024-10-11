9bd993b69dcfed343459e13cd4d306ad21c5a36d
Vladislav Pyatkov <vldpyatkov@gmail.com>
2024-10-11 15:13:52 +0300

    Added a massage about conflicted entries.

--- modules/transactions/src/main/java/org/apache/ignite/internal/tx/impl/HeapLockManager.java
+++ modules/transactions/src/main/java/org/apache/ignite/internal/tx/impl/HeapLockManager.java
@@ -74,7 +74,7 @@ public class HeapLockManager extends AbstractEventProducer<LockEvent, LockEventP
     /**
      * Table size. TODO make it configurable IGNITE-20694
      */
-    public static final int SLOTS = 531_072;
+    public static final int SLOTS = 1_000_000;
 
     /**
      * Empty slots.
