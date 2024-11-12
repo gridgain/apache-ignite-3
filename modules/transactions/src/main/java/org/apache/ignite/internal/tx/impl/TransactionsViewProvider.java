b45bfcaff3acc176171c44a1c54b53bce486cdc7
Pavel Pereslegin <xxtern@gmail.com>
2024-11-12 17:15:49 +0300

    Revert "IGNITE-23431 End-to-end test."
    This reverts commit 2a705845f948f88d821e2eb07889855eec6ca725.

--- modules/transactions/src/main/java/org/apache/ignite/internal/tx/impl/TransactionsViewProvider.java
+++ modules/transactions/src/main/java/org/apache/ignite/internal/tx/impl/TransactionsViewProvider.java
@@ -35,11 +35,7 @@ import org.jetbrains.annotations.Nullable;
 /**
  * {@code TRANSACTIONS} system view provider.
  */
-public class TransactionsViewProvider {
-    public static final String READ_ONLY = "READ ONLY";
-
-    public static final String READ_WRITE = "READ WRITE";
-
+class TransactionsViewProvider {
     private final Map<UUID, InternalTransaction> transactions;
 
     TransactionsViewProvider(Map<UUID, InternalTransaction> transactions) {
@@ -57,7 +53,7 @@ public class TransactionsViewProvider {
                 .<String>addColumn("STATE", stringType, tx -> deriveState(tx.state()))
                 .<String>addColumn("ID", stringType, tx -> tx.id().toString())
                 .<Instant>addColumn("START_TIME", timestampType, tx -> Instant.ofEpochMilli(tx.startTimestamp().getPhysical()))
-                .<String>addColumn("TYPE", stringType, tx -> tx.isReadOnly() ? READ_ONLY : READ_WRITE)
+                .<String>addColumn("TYPE", stringType, tx -> tx.isReadOnly() ? "READ ONLY" : "READ WRITE")
                 .<String>addColumn("PRIORITY", stringType, tx -> TransactionIds.priority(tx.id()).name())
                 .dataProvider(SubscriptionUtils.fromIterable(transactions.values()))
                 .build();
