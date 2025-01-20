531520a5ea6a13c5c5212c97ea93ebdc15197dd2
Alexey Scherbakov <alexey.scherbakoff@gmail.com>
2025-01-20 17:00:35 +0300

    IGNITE-24115 wip 14.

--- modules/storage-rocksdb/src/main/java/org/apache/ignite/internal/storage/rocksdb/index/RocksDbHashIndexStorage.java
+++ modules/storage-rocksdb/src/main/java/org/apache/ignite/internal/storage/rocksdb/index/RocksDbHashIndexStorage.java
@@ -125,15 +125,16 @@ public class RocksDbHashIndexStorage extends AbstractRocksDbIndexStorage impleme
     @Override
     public void put(IndexRow row) {
         busyNonDataRead(() -> {
-            //try {
+            try {
                 AbstractWriteBatch writeBatch = PartitionDataHelper.requireWriteBatch();
 
                 //writeBatch.put(indexCf.handle(), rocksKey(row), BYTE_EMPTY_ARRAY);
+                writeBatch.put(rocksKey(row), BYTE_EMPTY_ARRAY);
 
                 return null;
-            //} catch (RocksDBException e) {
-              //  throw new IgniteRocksDbException("Unable to insert data into hash index. Index ID: " + descriptor.id(), e);
-            //}
+            } catch (RocksDBException e) {
+                throw new IgniteRocksDbException("Unable to insert data into hash index. Index ID: " + descriptor.id(), e);
+            }
         });
     }
 
