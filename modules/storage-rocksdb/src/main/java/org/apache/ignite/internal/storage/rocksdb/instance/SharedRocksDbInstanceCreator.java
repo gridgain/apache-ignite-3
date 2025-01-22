0b564d76601703c043b63d8d60526d5aa740e806
Alexey Scherbakov <alexey.scherbakoff@gmail.com>
2025-01-22 15:21:11 +0300

    IGNITE-24115 wip 1

--- modules/storage-rocksdb/src/main/java/org/apache/ignite/internal/storage/rocksdb/instance/SharedRocksDbInstanceCreator.java
+++ modules/storage-rocksdb/src/main/java/org/apache/ignite/internal/storage/rocksdb/instance/SharedRocksDbInstanceCreator.java
@@ -94,7 +94,7 @@ public class SharedRocksDbInstanceCreator {
                     // Atomic flush must be enabled to guarantee consistency between different column families when WAL is disabled.
                     .setAtomicFlush(true)
                     .setListeners(List.of(flusher.listener()))
-                    .setWriteBufferManager(profile.writeBufferManager())
+                    //.setWriteBufferManager(profile.writeBufferManager())
                     // Don't flush on shutdown to speed up node shutdown as on recovery we'll apply commands from log.
                     .setAvoidFlushDuringShutdown(true)
             );
@@ -238,6 +238,7 @@ public class SharedRocksDbInstanceCreator {
     @SuppressWarnings("resource")
     static ColumnFamilyOptions sortedIndexCfOptions(byte[] cfName) {
         return new ColumnFamilyOptions()
+                .setWriteBufferSize(2L * 1024 * 1024 * 1024)
                 .setComparator(ColumnFamilyUtils.comparatorFromCfName(cfName))
                 .useCappedPrefixExtractor(AbstractRocksDbIndexStorage.PREFIX_WITH_IDS_LENGTH);
     }
