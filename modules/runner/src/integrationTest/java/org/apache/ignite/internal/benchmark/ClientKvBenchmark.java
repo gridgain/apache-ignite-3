9d8ad87e3d76de7e893049db21d319c3e1e4229c
Alexey Scherbakov <alexey.scherbakoff@gmail.com>
2025-04-17 14:28:46 +0300

    IGNITE-25099 Perf 8 - Bump partitions.

--- modules/runner/src/integrationTest/java/org/apache/ignite/internal/benchmark/ClientKvBenchmark.java
+++ modules/runner/src/integrationTest/java/org/apache/ignite/internal/benchmark/ClientKvBenchmark.java
@@ -70,7 +70,7 @@ public class ClientKvBenchmark extends AbstractMultiNodeBenchmark {
     @Param({"false"})
     private boolean fsync;
 
-    @Param({"32"})
+    @Param({"64"})
     private int partitionCount;
 
     private final AtomicInteger counter = new AtomicInteger();
