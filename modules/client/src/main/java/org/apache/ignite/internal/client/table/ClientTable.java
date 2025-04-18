9149c4d5b4e52941afff4d0a1d807372298aa369
Alexey Scherbakov <alexey.scherbakoff@gmail.com>
2025-04-18 12:04:37 +0300

    IGNITE-25099 Perf 21 - Ready

--- modules/client/src/main/java/org/apache/ignite/internal/client/table/ClientTable.java
+++ modules/client/src/main/java/org/apache/ignite/internal/client/table/ClientTable.java
@@ -492,7 +492,7 @@ public class ClientTable implements Table {
 
                         WriteContext ctx = new WriteContext();
                         // Force proxy mode for requests collocated with coordinator to reduce passed enlistment info on commit.
-                        ctx.pm = forOp;
+                        ctx.pm = tx0 != null && forOp != null && forOp.nodeConsistentId().equals(tx0.nodeName()) ? null : forOp;
 
                         return ch.serviceAsync(opCode,
                                         (opCh) -> tx0 == null || tx0.isReadOnly() || ctx.pm == null
