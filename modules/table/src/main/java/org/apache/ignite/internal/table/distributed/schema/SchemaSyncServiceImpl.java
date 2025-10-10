8f6e0ac9fcc9d2610df53aa584f7aafae9958fad
Vladislav Pyatkov <vldpyatkov@gmail.com>
2025-10-10 13:07:45 +0300

    Log.

--- modules/table/src/main/java/org/apache/ignite/internal/table/distributed/schema/SchemaSyncServiceImpl.java
+++ modules/table/src/main/java/org/apache/ignite/internal/table/distributed/schema/SchemaSyncServiceImpl.java
@@ -27,6 +27,7 @@ import org.apache.ignite.internal.logger.IgniteLogger;
 import org.apache.ignite.internal.logger.Loggers;
 import org.apache.ignite.internal.schema.SchemaSafeTimeTracker;
 import org.apache.ignite.internal.schema.SchemaSyncService;
+import org.apache.ignite.internal.thread.ThreadUtils;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -56,7 +57,7 @@ public class SchemaSyncServiceImpl implements SchemaSyncService {
                     long now = System.currentTimeMillis();
 
                     if (now - ts.getPhysical() > 500) {
-                        log.info("Too long schema waiting [start={}, now={}, duration={}ms, delayDuration={}ms]",
+                        ThreadUtils.dumpStack(log, "Too long schema waiting [start={}, now={}, duration={}ms, delayDuration={}ms]",
                                 formatTs(ts.getPhysical()), formatTs(now), now - ts.getPhysical(), delayDurationMs.getAsLong());
                     }
                 });
